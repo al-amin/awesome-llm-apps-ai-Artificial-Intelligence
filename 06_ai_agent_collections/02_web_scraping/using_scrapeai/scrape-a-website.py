50fb0bdd9957b931572963c8a6991760bdaf13a6
AL AMIN <al.amin1@allianz.de>
2024-12-27 09:45:54 +0100

    scraping updated

--- 06_ai_agent_collections/02_web_scraping/using_scrapeai/scrape-a-website.py
+++ 06_ai_agent_collections/02_web_scraping/using_scrapeai/scrape-a-website.py
@@ -1,26 +1,30 @@
-import nest_asyncio
-nest_asyncio.apply()
+# import nest_asyncio
+# nest_asyncio.apply()
 
 from scrapegraphai.graphs import SmartScraperGraph
 
 graph_config = {
     "llm": {
-        "model": "ollama/mistral",
+        "model": "ollama/llama3.2:latest",
         "temperature": 0,
         "format": "json",  # Ollama needs the format to be specified explicitly
         "base_url": "http://localhost:11434",  # set Ollama URL
     },
     "embeddings": {
-        "model": "ollama/nomic-embed-text",
+        "model": "ollama/nomic-embed-text:latest",
         "base_url": "http://localhost:11434",  # set Ollama URL
     }
 }
 
 smart_scraper_graph = SmartScraperGraph(
     prompt="List me all the articles",
-    source="https://allianz.com",
+    source="https://www.facebook.com/",
     config=graph_config
 )
 
 result = smart_scraper_graph.run()
+print("**************************************")
+print("\n")
 print(result)
+print("\n")
+print("**************************************")
